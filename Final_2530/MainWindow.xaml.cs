a5527b4213fd7e804dcbb6988de95d4445dfb220
thatsscience <nathan.ivy@outlook.com>
2015-04-29 03:37:24 -0600

    fixed position of game over score

--- Final_2530/MainWindow.xaml.cs
+++ Final_2530/MainWindow.xaml.cs
@@ -145,7 +145,7 @@ namespace Final_2530
         {
             // choose how long you want the application to run
             // Change the value in TimeSpan.FromSeconds to update how long the game lasts
-            time = TimeSpan.FromSeconds(165);
+            time = TimeSpan.FromSeconds(5);
 
             countDown = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
             {
